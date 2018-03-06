1817fb7cc06337cefe76f220e56a8f0a16a1717a
Shiv <ansalshiv@gmail.com>
2018-03-05 17:01:46 -0800

    fix direct

--- profpic.js
+++ profpic.js
@@ -23,7 +23,7 @@ bg.setAttribute('crossOrigin', 'anonymous');
 bg.src = 'default.png';
 var overlay = new Image();
 overlay.setAttribute('crossOrigin', 'anonymous');
-overlay.src = 'jnrs.png';    
+overlay.src = 'jnrs bigger.png';    
 overlay.onload = init;
 
 function init() {
